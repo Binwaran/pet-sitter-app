760dd10fb930ed3be38d2edbc7ac908833df66ed
Binwaran <binwaran.m@gmail.com>
2025-05-18 15:38:20 +0800

    fix: search bar mobile

---
+++ client/src/components/SitterGallery.js
@@ -0,0 +1,12 @@
+'use client'
+
+const SitterGallery = () => {
+  return (
+    <div className="w-full h-64 md:h-96 bg-gray-200 flex items-center justify-center">
+      {/* ในอนาคตสามารถเปลี่ยนเป็น carousel จริง ๆ ได้ */}
+      <span className="text-gray-500 text-xl">[ Image Gallery Placeholder ]</span>
+    </div>
+  )
+}
+
+export default SitterGallery
---
+++ client/src/components/StickyMobileCTA.js
@@ -0,0 +1,14 @@
+'use client'
+
+const StickyMobileCTA = () => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t shadow-lg p-4">
+      <button className="w-full bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 rounded-xl text-lg">
+        Book Now
+      </button>
+    </div>
+  )
+}
+
+export default StickyMobileCTA
+
