6c60710b6f73e241b50c1fd40ed9eed1118d90f2
Binwaran <73376855+Binwaran@users.noreply.github.com>
2025-05-30 12:01:31 +0000

    🔧 Fix send message routing and UI for ChatWindow

--- client/src/app/pet-owners/edit-pet/page.js
+++ client/src/app/pet-owners/edit-pet/page.js
@@ -1,4 +1,12 @@
-import { redirect } from "next/navigation";
+export const dynamic = 'force-dynamic'
+
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
+
+const petTypes = ["Dog", "Cat", "Other"];
+const sexes = ["Male", "Female", "Unknown"];
 
 export default function NotFound() {
   redirect("/");
