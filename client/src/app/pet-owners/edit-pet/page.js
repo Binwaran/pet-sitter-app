7cba77e6ee1acbac4e20731eb73d2808b33a0ae1
Binwaran <73376855+Binwaran@users.noreply.github.com>
2025-06-09 13:59:06 +0000

    edit pet-owner-edit-pet

--- client/src/app/pet-owners/edit-pet/page.js
+++ client/src/app/pet-owners/edit-pet/page.js
@@ -1,22 +1,16 @@
-"use client";
-export const dynamic = 'force-dynamic'
-import { redirect } from "next/navigation";
-import React, { useEffect, useState } from "react";
-"use client";
-export const dynamic = 'force-dynamic'
-import { useRouter, useSearchParams } from "next/navigation";
-import { useAuth } from "@/context/AuthContext";
+"use client"
 
-const petTypes = ["Dog", "Cat", "Other"];
-const sexes = ["Male", "Female", "Unknown"];
-import React, { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useAuth } from "@/context/AuthContext";
+export const dynamic = "force-dynamic"
 
-const petTypes = ["Dog", "Cat", "Other"];
-const sexes = ["Male", "Female", "Unknown"];
+import { redirect } from "next/navigation"
+import React, { useEffect, useState } from "react"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useAuth } from "@/context/AuthContext"
+
+const petTypes = ["Dog", "Cat", "Other"]
+const sexes = ["Male", "Female", "Unknown"]
 
 export default function NotFound() {
-  redirect("/");
-  return null;
+  redirect("/")
+  return null
 }
