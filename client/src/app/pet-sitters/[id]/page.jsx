3ac684532823779ee8d81262e348c3778b02df02
Binwaran <73376855+Binwaran@users.noreply.github.com>
2025-06-13 12:16:48 +0000

    feat: integrate MapSitter for user My Place section

--- client/src/app/pet-sitters/[id]/page.jsx
+++ client/src/app/pet-sitters/[id]/page.jsx
@@ -8,6 +8,7 @@ import { Toaster, toast } from 'sonner'
 import ImageCarousel from '@/components/ImageCarousel'
 import StickyCard from '@/components/StickyCard'
 import ReviewSection from '@/components/pet-sitters/ReviewSection'
+import MyPlaceSection from '@/components/pet-sitters/MyPlaceSection'
 
 const PetSitterDetails = () => {
   const { id } = useParams()
@@ -81,18 +82,7 @@ const PetSitterDetails = () => {
             </section>
 
             {/* My Place */}
-            <section className="mb-10">
-              <h2 className="text-3xl font-semibold mb-1">My Place</h2>
-              <p className="text-gray-700 mb-4 whitespace-pre-wrap text-lg sm:mb-10 leading-relaxed">{sitter.my_place}</p>
-              {sitter.lat && sitter.lng && (
-                <iframe
-                  className="rounded-lg w-full h-64"
-                  src={`https://www.google.com/maps?q=${sitter.lat},${sitter.lng}&z=15&output=embed`}
-                  allowFullScreen
-                  loading="lazy"
-                ></iframe>
-              )}
-            </section>
+            <MyPlaceSection sitter={sitter} />
           </div>
           <div className="w-full h-[2000px] my-15 py-10 ">
             <ReviewSection sitterId={sitter.user_id} averageRating={sitter.average_rating} />
